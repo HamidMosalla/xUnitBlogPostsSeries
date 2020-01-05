056679c4751bd3f7f1a72722d003fd3534c9f894
Hamid Mosalla <Xellarix@gmail.com>
2020-01-05 17:19:15 +0100

    add unit test projects

--- XUnit - Part 1/Controllers/HomeController.cs
+++ XUnit - Part 1/Controllers/HomeController.cs
@@ -11,13 +11,6 @@ namespace xUnitPartOne.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             ICollection<string> vs = null;
