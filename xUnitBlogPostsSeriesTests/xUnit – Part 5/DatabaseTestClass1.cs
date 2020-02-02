80b56af05217f1d901f878319482dad6f656706a
Hamid Mosalla <Xellarix@gmail.com>
2020-02-02 22:56:58 +0100

    define collection definition and ICollectionFixture

---
+++ xUnitBlogPostsSeriesTests/xUnit – Part 5/DatabaseTestClass1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xUnitBlogPostsSeriesTests.xUnit___Part_5
+{
+    [CollectionDefinition("Context collection")]
+    public class InMemoryDbContextFixtureCollection : ICollectionFixture<InMemoryDbContextFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
---
+++ xUnitBlogPostsSeriesTests/xUnit – Part 5/UsingCollectionDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xUnitBlogPostsSeriesTests.xUnit___Part_5
+{
+    [Collection("Context collection")]
+    public class ContextTestClass1
+    {
+        InMemoryDbContextFixture fixture;
+
+        public ContextTestClass1(InMemoryDbContextFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+    }
+
+    [Collection("Context collection")]
+    public class ContextTestClass2
+    {
+        // ...
+    }
+}
