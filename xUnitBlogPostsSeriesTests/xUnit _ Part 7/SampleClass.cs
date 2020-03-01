d6d177f6ef2f2eb4f6dbbcb70473285166e02277
Hamid Mosalla <Xellarix@gmail.com>
2020-03-01 21:30:45 +0100

    add sample for using trait in xUnit

---
+++ xUnitBlogPostsSeriesTests/xUnit _ Part 7/SampleClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xUnitBlogPostsSeriesTests.xUnit___Part_7
+{
+    public class SampleClass
+    {
+        public int Add(int left, int right) => left + right;
+    }
+}
---
+++ xUnitBlogPostsSeriesTests/xUnit _ Part 7/SampleClassTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace xUnitBlogPostsSeriesTests.xUnit___Part_7
+{
+    public class SampleClassTests
+    {
+        private SampleClass _sut;
+
+        public SampleClassTests()
+        {
+            _sut = new SampleClass();
+        }
+
+        [Fact]
+        public void SampleClass_Add_Always_ReturnsTheCorrectResult()
+        {
+            Assert.True(_sut.Add(2, 2) == 4);
+        }
+
+        [Fact]
+        [Trait("Category", "Add")]
+        public void Add_Always_ReturnsTheCorrectResult()
+        {
+            Assert.True(_sut.Add(2, 2) == 4);
+        }
+
+        [Fact]
+        [Trait("Category", "Add")]
+        public void Add_WithZeroArguments_ReturnsZero()
+        {
+            Assert.True(_sut.Add(0, 0) == 0);
+        }
+    }
+}
