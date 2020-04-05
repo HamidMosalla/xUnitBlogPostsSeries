c1afbe4297b70ad4d4f885f011e6d77be7dc9b34
Hamid Mosalla <Xellarix@gmail.com>
2020-04-05 20:25:07 +0200

    add sample for using Trait

--- xUnitBlogPostsSeriesTests/xUnit _ Part 7/SampleClassTests.cs
+++ xUnitBlogPostsSeriesTests/xUnit _ Part 7/SampleClassTests.cs
@@ -21,15 +21,29 @@ namespace xUnitBlogPostsSeriesTests.xUnit___Part_7
         }
 
         [Fact]
-        [Trait("Category", "Add")]
+        [Trait("UI", "Front")]
         public void Add_Always_ReturnsTheCorrectResult()
         {
             Assert.True(_sut.Add(2, 2) == 4);
         }
 
         [Fact]
-        [Trait("Category", "Add")]
+        [Trait("UI", "Front")]
         public void Add_WithZeroArguments_ReturnsZero()
+        {
+            Assert.True(_sut.Add(0, 0) == 0);
+        }
+
+        [Fact]
+        [Trait("UI", "Back")]
+        public void Add_Always_ReturnsTheCorrectResult2()
+        {
+            Assert.True(_sut.Add(2, 2) == 4);
+        }
+
+        [Fact]
+        [Trait("UI", "Back")]
+        public void Add_WithZeroArguments_ReturnsZero2()
         {
             Assert.True(_sut.Add(0, 0) == 0);
         }
